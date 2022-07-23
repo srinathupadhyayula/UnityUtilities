e1a4338b9f1955baf3fdff9025613ae51651b024
Srinath Upadhyayula <srinathupadhyayula@gmail.com>
2022-07-23 15:46:24 +0530

    AssetPath attribute by Byron Mayne that internally converts asset to its string path.

---
+++ Runtime/Utilities/Attributes/AssetPathAttribute/AssetPathTypes.cs
@@ -0,0 +1,23 @@
+﻿namespace Utilities.Attributes
+{
+    /// /// <summary>
+    /// <para> A enum containing all the types of paths we can watch.</para>
+    /// </summary>
+    /// <remarks>
+    /// <para>Author: Byron Mayne</para>
+    /// <see cref="https://github.com/ByronMayne"/>
+    /// </remarks>
+    public enum AssetPathTypes
+    {
+        /// <summary>
+        /// The path will be contained within the 'Asset/*' directory.
+        /// </summary>
+        Project,
+
+        /// <summary>
+        /// The path will be contained within a resources folder.
+        /// </summary>
+        Resources
+    }
+
+}
